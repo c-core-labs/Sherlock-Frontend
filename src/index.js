79e222f03a1e929dcccdd0f8beb057b3430c3b05
Jeremy T. Hetzel <jthetzel@gmail.com>
2021-03-12 13:28:20 -0330

    chore: Analytics (#19)
    * docs: Update COG link
    
    * chore: Add analytics measurement id

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import ReactGA from 'react-ga'
+
+import { measurementId } from './config'
+
+ReactGA.initialize(measurementId, { debug: false })
+ReactGA.pageview(window.location.pathname + window.location.search)
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,4 +20,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
